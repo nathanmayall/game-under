dc06ec88a0942ede02835e76f56df83fb6fa2136
Nathan M <70609396+nathanmayall@users.noreply.github.com>
2021-07-26 22:41:18 +0100

    edit gameca rd to be more readable with price formatting

--- components/GameCard.js
+++ components/GameCard.js
@@ -3,18 +3,16 @@ import styles from "../styles/GameCard.module.css";
 const gameCard = ({ game }) => {
   const { name, steamID, is_free, hasPriceOverview } = game;
 
+  const formattedPrice =
+    new Intl.NumberFormat("en-GB", {
+      style: "currency",
+      currency: hasPriceOverview?.currency || "USD",
+    }).format(hasPriceOverview?.final / 100) || null;
+
   return (
     <div className={styles.card}>
       <h2>{name}</h2>
-      <p>
-        Price:{" "}
-        {is_free
-          ? "Free ✔!"
-          : new Intl.NumberFormat("en-GB", {
-              style: "currency",
-              currency: hasPriceOverview?.currency || "USD",
-            }).format(hasPriceOverview?.final / 100)}
-      </p>
+      <p>Price: {is_free ? "Free ✔!" : formattedPrice}</p>
       <small>Steam ID: {steamID}</small>
     </div>
   );
