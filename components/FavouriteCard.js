0e94cc0ca1656d0d331e33653f69f15b5e091656
Nathan M <70609396+nathanmayall@users.noreply.github.com>
2021-08-04 20:49:24 +0100

    add favourite card to user page

---
+++ components/FavouriteCard.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styles from "../styles/FavouriteCard.module.css";
+
+const FavouriteCard = ({ appID }) => {
+  const [favData, setFavData] = useState({});
+
+  useEffect(async () => {
+    const { data } = await axios(
+      `https://api.steamapis.com/market/app/${appID}?api_key=zSQo-hIrr3nUU5T__NbF8Bc_Y1w`
+    );
+    setFavData(data);
+    console.log(favData);
+  }, []);
+
+  const {
+    header_image,
+    name,
+    short_description,
+    developers,
+    publishers,
+    platforms,
+  } = favData;
+
+  return (
+    <div className={styles.card}>
+      {favData && (
+        <>
+          <img src={header_image} alt="" />
+          <p>{name}</p>
+        </>
+      )}
+      <small>{appID}</small>
+    </div>
+  );
+};
+
+export default FavouriteCard;
