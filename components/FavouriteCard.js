f5d5bc79c63c2e50a644d101db750dea5c058b86
Nathan M <70609396+nathanmayall@users.noreply.github.com>
2021-08-14 01:40:20 +0100

    revamp favourite page

--- components/FavouriteCard.js
+++ components/FavouriteCard.js
@@ -3,18 +3,32 @@ import { useEffect, useState } from "react";
 import styles from "../styles/FavouriteCard.module.css";
 import priceFormatter from "../utils/PriceFormatter";
 
+import Image from "next/image";
+
+import placeholder from "../public/placeholder.jpg";
+
 const FavouriteCard = ({ appID }) => {
   const [favData, setFavData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     getFavInfo(appID);
   }, [appID]);
 
   const getFavInfo = async (appID) => {
-    const { data } = await axios(
-      `https://api.steamapis.com/market/app/${appID}?api_key=zSQo-hIrr3nUU5T__NbF8Bc_Y1w`
-    );
-    setFavData(data);
+    setLoading(true);
+    try {
+      const {
+        data: { result },
+      } = await axios(`/api/games/${appID}`);
+      setFavData(result);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+      console.log(error);
+    }
   };
 
   const {
@@ -25,23 +39,34 @@ const FavouriteCard = ({ appID }) => {
     publishers,
     platforms,
     price_overview,
+    background,
   } = favData;
 
   return (
-    <div className={styles.card}>
-      {favData && (
-        <>
-          <img src={header_image} alt="" className={styles.image} />
-
+    <>
+      {!error && !loading && header_image && favData ? (
+        <div className={styles.card}>
+          <Image
+            src={header_image}
+            alt="Favourite Card"
+            layout="intrinsic"
+            objectFit=""
+            width={350}
+            height={175}
+            placeholder={blur}
+            className={styles.image}
+          />
           <div className={styles.text}>
             <p>{name}</p>
-            {price_overview && <h2>price: {priceFormatter(price_overview)}</h2>}
+            {price_overview && <h2>Price: {priceFormatter(price_overview)}</h2>}
             <small>{appID}</small>
           </div>
           <button className={styles.fav}>Favourite</button>
-        </>
+        </div>
+      ) : (
+        <div className={styles.card}>Loading...</div>
       )}
-    </div>
+    </>
   );
 };
 
